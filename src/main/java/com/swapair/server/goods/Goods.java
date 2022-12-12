1730b13101970f338d359da72cfb943104d0e531
dingding-21 <phj8221@naver.com>
2022-12-12 16:19:02 +0900

    [FEAT] Post, Goods column추가

--- src/main/java/com/swapair/server/goods/Goods.java
+++ src/main/java/com/swapair/server/goods/Goods.java
@@ -1,12 +1,17 @@
 package com.swapair.server.goods;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.swapair.server.post.have.HaveGoods;
 import com.swapair.server.post.want.WantGoods;
 import lombok.*;
+import org.apache.tomcat.jni.Local;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Builder
@@ -33,4 +38,21 @@ public class Goods {
     @JsonIgnore
     @OneToMany(mappedBy = "goods", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<HaveGoods> haveGoodsList = new ArrayList<>();
+
+    @Column(nullable = false)
+    private Long goodsPrice;
+
+    @Column(nullable = false)
+    private Long goodsPrice1;
+
+    @Column(nullable = false)
+    private Long goodsPrice2;
+
+    @Column(nullable = false)
+    private Long goodsPrice3;
+
+    @Column
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime storageReferenceDate;
 }
